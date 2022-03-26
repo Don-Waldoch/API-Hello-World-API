f925b46998952909efbcf9b84818b31a2a28e185
unknown <waldoch.don@gmail.com>
2022-03-26 13:13:29 -0500

    Data Seeded

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
+
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
 
 // ROUTES
 app.get('/', (req, res) => {
